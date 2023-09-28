5df3b89685db50167a7dbb8b6e0aec228b699c7f
lucasfeklein <lucas.fklein@hotmail.com>
2023-09-28 15:52:55 +0700

    cards

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
 function App() {
   return (
     <div className="container">
-      <div className="card-container"></div>
+      <div className="cards-container">
+        <div className="front-card">
+          <img src="./card-logo.svg" />
+          <p>0000 0000 0000 0000</p>
+          <div style={{ display: "flex", justifyContent: "space-between" }}>
+            <p>Name</p>
+            <p>Date</p>
+          </div>
+        </div>
+        <div className="back-card"></div>
+      </div>
       <div></div>
     </div>
   );
