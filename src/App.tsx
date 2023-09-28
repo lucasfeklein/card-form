2c47cf485e87452bc88f32aa5f68d5de44c3633e
lucasfeklein <lucas.fklein@hotmail.com>
2023-09-28 14:56:34 +0700

    container background

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,10 @@
 function App() {
-  return <>a</>;
+  return (
+    <div className="container">
+      <div className="card-container"></div>
+      <div></div>
+    </div>
+  );
 }
 
 export default App;
